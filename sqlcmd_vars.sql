eaf0d677669f39d2b11774b37f0fbcdd978ccdb3
Christian Amezcua <33039101+chrisamz@users.noreply.github.com>
2024-07-02 13:23:26 -0700

    Create sqlcmd_vars.sql

---
+++ sqlcmd_vars.sql
@@ -0,0 +1,11 @@
+-- sqlcmd_vars.sql
+-- Define SQLCMD variables
+
+-- Variable for the database name
+:setvar DatabaseName "YourDatabaseName"
+
+-- Variable for the backup directory
+:setvar BackupDirectory "C:\Backups"
+
+-- Variable for the server name
+:setvar ServerName "YourServerName"
